99e92523594af7de5e26b83b3db573c5d29d5644
Kathy Pegion <kpegion@esplab-0-2.localdomain>
2025-06-27 10:44:12 -0500

    fixes for binder

--- mimimal_app.py
+++
@@ -1,4 +0,0 @@
-import intake
-
-cat = intake.open_catalog("obs.yaml")
-print(cat._entries["obs/gridded/atm/precip/monthly/CPC-UNI-HIRES"].metadata)
--- test_app.py
+++
@@ -1,9 +0,0 @@
-import intake
-
-cat = intake.open_catalog("top_catalog.yaml")
-print("Top-level entries:", list(cat))
-
-for key in cat:
-    print(f"--- {key} ---")
-    subcat = cat[key]()
-    print("Subentries:", list(subcat))
